13a0161bfeb3da77443b2cabb3bc93f5440f8219
Virginia García <vickkygm1@gmail.com>
2018-02-28 14:31:45 -0600

    j

---
+++ cookies/__init__.py
@@ -0,0 +1,8 @@
+
+
+def cookies_function(love):
+  if love == 1 :
+    print("I love cookies!!")
+  else:
+    print("I'm not a cool person :( !!")
+  return
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='cookies',
+      version='0.3',
+      description='Cool person or not',
+      url='https://github.com/vickygm/cookies',
+      author='Moana',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['cookies'],
+      zip_safe=False)
+
